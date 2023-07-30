ef1891a62e3f2bdcbc578c9dc41e6e6bcd1fda66
김정규 <111268088+gyu0918@users.noreply.github.com>
2023-07-30 22:43:10 +0900

    WhoAreYou

---
+++ mini/minishell/test.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/fcntl.h>
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	src_len;
+
+	src_len = ft_strlen(src);
+	if (!dst || !src)
+		return (0);
+	if (dstsize == 0)
+		return (src_len);
+	i = 0;
+	while (i < src_len && i < dstsize - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = ' ';
+	return (src_len);
+}
+
+void	test(int i, int j, int k, int l, int m)
+{
+	printf("dd");
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+  
+  char str[] = "memmove can be very useful";
+  ft_strlcpy(&str[3], "s112dfsfdsfsdfsdfsdfsdfsdddd", 28);
+  test()
+  printf("%s\n", str);
+  return 0;
+}

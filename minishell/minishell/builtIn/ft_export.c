d452ba0094bb09185010016566c0e37a6d6459fc
Junggyu Kim <kjk1526@naver.com>
2023-09-03 15:13:15 +0900

    dddd

--- minishell/minishell/builtIn/ft_export.c
+++ minishell/minishell/builtIn/ft_export.c
@@ -12,133 +12,12 @@
 
 #include "../minishell.h"
 
-int	check_len(char **env)
-{
-	int	i;
-	int	count;
-
-	count = 0;
-	i = 0;
-	while (env[i])
-	{
-		if (env[i][0] == '_' && env[i][1] == '=')
-			i++;
-		else
-		{
-			i++;
-			count++;
-		}
-	}
-	return (count);
-}
-
-char	**copy_env(char **env, int env_len)
-{
-	char	**tmp;
-	int		i;
-	int		j;
-	int		k;
-
-	tmp = malloc(sizeof(char *) * env_len);
-	i = 0;
-	k = 0;
-	while (env[i])
-	{
-		//printf("env[i][0] = %c env[i][1] = %c\n", env[i][0], env[i][1]);
-		if (env[i][0] == '_' && env[i][1] == '=')
-			i++;
-		else
-		{
-			env_len = ft_strlen(env[i]);
-			tmp[k] = malloc(sizeof(char) * env_len + 1);
-			j = -1;
-			while (env[i][++j])
-				tmp[k][j] = env[i][j];
-			tmp[k][j] = '\0';
-			k++;
-			i++;
-		}
-	}
-	return (tmp);
-}
-
-// char	**sort_env(char **env)
-// {
-// 	int		i;
-// 	int		j;
-// 	char	*temp;
-// 	int		len;
-
-// 	i = -1;
-// 	len = check_len(env);
-// 	printf("len = %d\n", len);
-// 	while (++i < len)
-// 	{
-// 		j = i + 1;
-// 		while (j < len)
-// 		{
-// 			//printf("env[i] = %s env[j] = %s\n", env[i], env[j]);	
-// 			if (ft_strcmp(env[i], env[j]) > 0 && env[i] && env[j])
-// 			{
-// 				temp = env[i];
-// 				env[i] = env[j];
-// 				env[j] = temp;
-// 			}
-// 			j++;
-// 		}
-// 	}
-// 	return (env);
-// }
-
-// void	declare_print(t_info *info)
-// {
-// 	char	**tmp_env;
-// 	int	i;
-// 	int	len;
-
-// 	len = check_len(info->envp);
-// 	tmp_env = copy_env(info->envp, len);
-// 	tmp_env = sort_env(tmp_env);
-// 	i = -1;
-// 	while (tmp_env[++i])
-// 		printf("%s\n", tmp_env[i]);
-// 	free(tmp_env);
-// }
-
-// t_env	*sort_env(t_env	*env)
-// {
-// 	int		i;
-// 	int		j;
-// 	char	*temp;
-// 	int		len;
-
-// 	i = -1;
-// 	len = check_len(env);
-// 	printf("len = %d\n", len);
-// 	while (++i < len)
-// 	{
-// 		j = i + 1;
-// 		while (j < len)
-// 		{
-// 			//printf("env[i] = %s env[j] = %s\n", env[i], env[j]);	
-// 			if (ft_strcmp(env[i], env[j]) > 0 && env[i] && env[j])
-// 			{
-// 				temp = env[i];
-// 				env[i] = env[j];
-// 				env[j] = temp;
-// 			}
-// 			j++;
-// 		}
-// 	}
-// 	return (env);
-// }
 
 void	declare_print(t_info *info)
 {
 	t_env	*tmp_env;
 
 	tmp_env = info->envp_head;
-	//tmp_env = sort_env(tmp_env);
 	while (tmp_env)
 	{
 		if (tmp_env->name[0] == '_' && tmp_env->name[1] == '\0')
@@ -157,6 +36,21 @@ void	declare_print(t_info *info)
 			tmp_env = tmp_env->next;
 		}
 	}
+}
+
+void	pick_name_val2(t_env *tmp, char *cmd, int *nlen, int *vlen)
+{
+	int	i;
+
+	while (cmd[*nlen + 1 + *vlen] != '\0')
+		(*vlen)++;
+	tmp->value = malloc(sizeof(char) * (*vlen) + 1);
+	if (!tmp->value)
+		exit(1);
+	i = -1;
+	while (++i < *vlen)
+		tmp->value[i] = cmd[i + (*nlen) + 1];
+	tmp->value[i] = '\0';
 }
 
 int		pick_name_val(t_env *tmp, char *cmd)
@@ -178,15 +72,7 @@ int		pick_name_val(t_env *tmp, char *cmd)
 	tmp->name[i] = '\0';
 	if (cmd[nlen + 1] == '\0')
 		return (0);
-	while (cmd[nlen + 1 + vlen] != '\0')
-		vlen++;
-	tmp->value = malloc(sizeof(char) * vlen + 1);
-	if (!tmp->value)
-		exit(1);
-	i = -1;
-	while (++i < vlen)
-		tmp->value[i] = cmd[i + nlen + 1];
-	tmp->value[i] = '\0';
+	pick_name_val2(tmp, cmd, &nlen, &vlen);
 	return (1);
 }
 
@@ -204,6 +90,11 @@ int		check_cmd(char *cmd)
 	return (1);
 }
 
+void	free_tmp(t_env tmp)
+{
+	free(tmp.name);
+	free(tmp.value);
+}
 
 // export a                            #key값만 생성
 // export b=                           #value에 아무 값 없음
@@ -234,92 +125,9 @@ int	ft_export(char **cmd, t_info *info)
 			if (pick_name_val(&tmp, cmd[i]) == 0)
 				tmp.value = 0;
 			change_env_export(info, tmp.name, tmp.value, 1);
-			free(tmp.name);
-			free(tmp.value);
+			free_tmp(tmp);
 		}
 	}
 	return (0);
 }
 
-
-
-// int	checkvalid(char *cmd)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (cmd[i])
-// 	{
-// 		if ((ft_isalnum(cmd[i]) == 0 && cmd[i] != '\''
-// 				&& cmd[i] != '"' && cmd[i] != '_' && cmd)
-// 			|| ft_isdigit(cmd[0]) == 1 || cmd[0] == '=')
-// 		{
-// 			puterror(cmd, "not a valid identifier");
-// 			return (0);
-// 		}
-// 		i++;
-// 		if (cmd[i] == '=')
-// 			break ;
-// 	}
-// 	return (1);
-// }
-
-// void	export_print_2(char **env, int i, int j)
-// {
-// 	char	*temp;
-
-// 	if (ft_strcmp(env[i], env[j]) > 0 && env[i] && env[j])
-// 	{
-// 		temp = env[i];
-// 		env[i] = env[j];
-// 		env[j] = temp;
-// 	}
-// }
-
-// void	export_print(void)
-// {
-// 	int		len;
-// 	char	**env;
-// 	int		i;
-// 	int		j;
-
-// 	i = 0;
-// 	len = splitlen(g_data->env);
-// 	env = array_dup();
-// 	if (env == NULL)
-// 		return ;
-// 	while (i < len)
-// 	{
-// 		j = i + 1;
-// 		while (++j < len)
-// 			export_print_2(env, i, j);
-// 		if (env[i])
-// 			format_env(env[i]);
-// 		i++;
-// 	}
-// }
-
-// int	export(char **cmd)
-// {
-// 	int		i;
-// 	char	*tmp;
-
-// 	i = 1;
-// 	if (!cmd[1])
-// 	{
-// 		export_print();
-// 		return (0);
-// 	}
-// 	while (cmd[i])
-// 	{
-// 		if (cmd[i][0] == '_' && cmd[i][1] == '=')
-// 			return (0);
-// 		if (!checkvalid(ft_ecrase_q(cmd[i])))
-// 			return (1);
-// 		tmp = ft_ecrase_q(cmd[i]);
-// 		my_setenv(tmp);
-// 		free(tmp);
-// 		i++;
-// 	}
-// 	return (0);
-// }
